28d44ae1a1eca7f610ce5c81778e14cc6e013c81
Santiago30032 <santiago30032@gmail.com>
2023-08-09 18:12:10 -0500

    segundo commit

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
+
 # Create your views here.
 
 def home(request):
@@ -11,7 +12,6 @@ def store(request):
     return render(request,"core/store.html")
 def contact(request):
     return render(request,"core/contact.html")
-def blog(request):
-    return render(request,"core/blog.html")
+
 def sample(request):
     return render(request,"core/sample.html")
